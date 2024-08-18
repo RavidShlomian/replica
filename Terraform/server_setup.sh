f7f4f12e68193aa02ca02e2c53cf0bd5369a28b4
Ravid Shlomian <ravidshlomian@Ravids-MacBook-Pro.local>
2024-08-18 20:32:50 +0300

    added logs to the docker commands

--- Terraform/server_setup.sh
+++ Terraform/server_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 echo "starting user_data script" > /var/log/user-data.log
 
 # Update the system
@@ -11,10 +10,6 @@ uijXOzGcGbQ3R3DGCo/gLYdO55Cysgzer0CrBE+5YDGya+agif6nvot5+/Ea+J6Iw2GqmTxP3g8YNon/
 kHWIzXO5AJsxBLJckrfnkiDJbZu4V2GXqLcT8ofDtHJQVodVVjTuQxYDnQed5rslvo9pLbDqB7zvo7ktWpoY2i2vSUXMAdz7Y96cNy1aq
 UmKKheMBm2KtIAfEuuFvPL29ErjAxTL5in7qIqKzbZzadMKshBheGCFi6/SUFFAWtK2OoZfp07" >> /home/ec2-user/.ssh/authorized_keys
 
-# Create files for testing
-touch /home/ec2-user/abc.txt
-touch /home/ec2-user/abcdf.txt
-
 # Install Docker
 sudo yum install docker -y
 
@@ -22,14 +17,15 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Pull the latest Nginx image
+# Pull the latest Nginx image from the public dockerhub (without any need for login)
 sudo docker pull nginx:latest
 
 # Download configuration files
 curl -o /home/ec2-user/nginx.conf https://raw.githubusercontent.com/RavidShlomian/Moveo-HLS-Task/feature/Terraform/nginx.conf
 curl -o /home/ec2-user/Dockerfile https://raw.githubusercontent.com/RavidShlomian/Moveo-HLS-Task/feature/Terraform/Dockerfile
-
+mkdir /home/ec2-user/logs
+touch /home/ec2-user/logs/build.txt /home/ec2-user/logs/run.txt
 # Build the Docker image with the -f option because the script run from the directory /var/liv/cloud/instances/instance-id/scripts 
-sudo docker build -f /home/ec2-user/Dockerfile -t /home/ec2-user nginx
-# Run the Nginx container with the custom configuration
-sudo docker run --name nginx -d -p 80:80 -v /home/ec2-user/nginx.conf:/etc/nginx/nginx.conf:ro nginx
+sudo docker build -f /home/ec2-user/Dockerfile -t /home/ec2-user nginx >> /home/ec2-user/logs/build.txt
+# Run the Nginx container with the custom configuration file (nginx.conf) as read only beacuse i haven't saw any need for more than that for this scenario
+sudo docker run --name nginx -d -p 80:80 -v /home/ec2-user/nginx.conf:/etc/nginx/nginx.conf:ro nginx >> /home/ec2-user/logs/run.txt
