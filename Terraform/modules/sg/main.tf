2a813bbde006578ca559fe9c7c5f42d08e249c8c
Ravid Shlomian <ravidshlomian@Ravids-MacBook-Pro.local>
2024-08-24 17:05:33 +0300

    added the alb dns name as output

--- Terraform/modules/sg/main.tf
+++ Terraform/modules/sg/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   ingress {
-    description      = "HTTP"
+    description      = "HTTPS"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
